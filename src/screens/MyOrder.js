56bab91dec0f9356b589812ae0308666eff54eb1
Shivam Mishra <shivammishra7613@gmail.com>
2023-08-25 16:44:57 +0530

    Update MyOrder.js

--- src/screens/MyOrder.js
+++ src/screens/MyOrder.js
@@ -7,7 +7,7 @@ export default function MyOrder() {
 
   const fetchMyOrder = async () => {
     console.log(localStorage.getItem("userEmail"));
-    await fetch("/api/myOrderData", {
+    await fetch("https://affable.onrender.com/api/myOrderData", {
       // credentials: 'include',
       // Origin:"http://localhost:3000/login",
       method: "POST",
