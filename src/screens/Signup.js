eb2673bf880c2db4be4f647e8d19035babf8da40
Shivam Mishra <shivammishra7613@gmail.com>
2023-08-25 16:38:00 +0530

    Update Signup.js

--- src/screens/Signup.js
+++ src/screens/Signup.js
@@ -10,7 +10,7 @@ export default function SignUp() {
         
         console.log(JSON.stringify({name:credentials.name,email:credentials.email,password:credentials.password,location:credentials.location}))
         try {
-          const response = await fetch("http://localhost:5000/api/creatuser",{
+          const response = await fetch("/api/creatuser",{
             method:"POST",
             headers:{
               'Content-Type':'application/json'
